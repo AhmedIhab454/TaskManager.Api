58c46badb3b9b1a01aae7bcf6438033626d44b8b
AhmedIhab <ahmeddehab454@gmail.com>
2025-12-20 23:23:20 +0200

    Add project files.

---
+++ TaskManager.Api/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TaskManager.Api.Models;
+
+namespace TaskManager.Api.Data
+{
+    public class AppDbContext: DbContext
+    {
+        
+            public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+            {
+            }
+            public DbSet<User> Users { get; set; } = null!;
+            public DbSet<TaskItem> TaskItems { get; set; } = null!;
+        
+    }  
+}
---
+++ TaskManager.Api/Models/TaskItem.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TaskManager.Api.Models
+{
+    public class TaskItem
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; } = null;
+
+        public string? Description { get; set; }
+
+        public bool IsCompleted { get; set; } = false;
+        [Required]
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        [Required]
+        public int UserId { get; set; }
+
+
+        [ForeignKey("UserId")]
+        public User User { get; set; } = null;
+    }
+}
