382182b8c5a8d0fcef46041b44ce70a0dad7e941
AhmedIhab <ahmeddehab454@gmail.com>
2025-12-23 17:21:51 +0200

    add Loging Endpoint

--- TaskManager.Api/Controllers/AuthController.cs
+++ TaskManager.Api/Controllers/AuthController.cs
@@ -6,7 +6,9 @@ using TaskManager.Api.Models; //=>User Entity
 using Microsoft.EntityFrameworkCore; //efcore methods
 using System.Security.Cryptography; // password hashing 
 using System.Text; //encoding 
-
+using Microsoft.IdentityModel.Tokens; // JWT
+using System.IdentityModel.Tokens.Jwt; // JWT
+using System.Security.Claims; // JWT Claims
 
 
 namespace TaskManager.Api.Controllers
@@ -53,8 +55,63 @@ namespace TaskManager.Api.Controllers
             await _dbContext.SaveChangesAsync();
             return Ok("User registered successfully.");
 
+        }
 
+        [HttpPost("Login")]
+        public async Task<ActionResult> Login(LoginDto loginDto)
+        {
 
+            var user = await _dbContext.Users.FirstOrDefaultAsync(u => u.Username == loginDto.UsreName);
+            if (user == null)
+            {
+                return Unauthorized("Invalid username or password.");
+            }
+
+            // Hash the provided password   
+            using var sha256 = SHA256.Create();
+            byte[] passwordBytes = Encoding.UTF8.GetBytes(loginDto.Password);
+            byte[] hashedBytes = sha256.ComputeHash(passwordBytes);
+            string passwordHash = Convert.ToBase64String(hashedBytes);
+            // Compare hashes
+            if (user.PasswordHash != passwordHash)
+            {
+                return Unauthorized("Invalid username or password.");
+            }
+
+            //create claims
+            var claims = new[]
+            {
+                 new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()), // User ID
+                 new Claim(ClaimTypes.Name, user.Username) // Username
+            };
+
+            var key = new SymmetricSecurityKey(
+                   Encoding.UTF8.GetBytes(
+                       HttpContext.RequestServices
+                           .GetRequiredService<IConfiguration>()
+                           ["JwtSettings:SecretKey"]!
+                   )
+               );
+
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+            var token = new JwtSecurityToken(
+                issuer: HttpContext.RequestServices
+                           .GetRequiredService<IConfiguration>()
+                           ["JwtSettings:Issuer"],
+                audience: HttpContext.RequestServices
+                           .GetRequiredService<IConfiguration>()
+                           ["JwtSettings:Audience"],
+                claims: claims,
+                       expires: DateTime.UtcNow.AddMinutes(
+            int.Parse(
+                HttpContext.RequestServices.GetRequiredService<IConfiguration>()["JwtSettings:ExpiryMinutes"]! )),
+                signingCredentials: creds
+            );
+
+            return Ok(new
+            {
+                token = new JwtSecurityTokenHandler().WriteToken(token)
+            });
         }
     }
 }
---
+++ TaskManager.Api/DTOs/LoginDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TaskManager.Api.DTOs
+{
+    public class LoginDto
+    {
+        [Required]
+        public string UsreName { get; set; } = null!;
+
+        [Required]
+        public string Password { get; set; } = null!;
+    }
+}
